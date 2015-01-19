8b445754fbb42191e828df253cde945aee4461ce
mohammedfatahalla <mohammedfatahalla@gmail.com>
2015-01-19 05:38:06 -0800

    Update repeatelements.php

--- repeatelements.php
+++ repeatelements.php
@@ -1,16 +1,25 @@
-<?php
+/*
 
+snippest code to find repeated elements in an array
+author:- eng mohammed fatahalla
+my email :- mohammedfatahalla@gmail.com
+
+*/
+
+<?php
+//initialization of an array
 $a= array(1,2,2,3,3,4,5,6,6);
 
 function dupelement($a= array(1,2,2,3,3,4,5,6,6)){
 	 
 	 $i=0;
+	 //loop for searching about the repeated elements in the whole array
      for ($i=0; $i < count($a); $i++) { 
      	# code...
 
      	 if ($a[$i]===$a[$i+1]) {
      	 	# code...
-
+       //printing the repeated elements
      	 	echo $a[$i];
 
 
